1092d7863da3001eaf34560a6b04fef7a6fdb1db
kingnoel <k.godwin_379@contactbetrbeta.onmicrosoft.com>
2023-09-15 11:26:03 +0100

    events working

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -30,7 +30,11 @@ export default function Calculator() {
         <button type="button" onClick={buttonClick} className="items">
           %
         </button>
-        <button type="button" onClick={buttonClick} className="items orange">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items orangeColor"
+        >
           ÷
         </button>
         <button type="button" onClick={buttonClick} className="items">
@@ -42,7 +46,11 @@ export default function Calculator() {
         <button type="button" onClick={buttonClick} className="items">
           9
         </button>
-        <button type="button" onClick={buttonClick} className="items orange">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items orangeColor"
+        >
           x
         </button>
         <button type="button" onClick={buttonClick} className="items">
@@ -54,7 +62,11 @@ export default function Calculator() {
         <button type="button" onClick={buttonClick} className="items">
           6
         </button>
-        <button type="button" onClick={buttonClick} className="items orange">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items orangeColor"
+        >
           -
         </button>
         <button type="button" onClick={buttonClick} className="items">
@@ -66,16 +78,28 @@ export default function Calculator() {
         <button type="button" onClick={buttonClick} className="items">
           3
         </button>
-        <button type="button" onClick={buttonClick} className="items orange">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items orangeColor"
+        >
           +
         </button>
-        <button type="button" onClick={buttonClick} className="items long">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items longLength"
+        >
           0
         </button>
         <button type="button" onClick={buttonClick} className="items">
           .
         </button>
-        <button type="button" onClick={buttonClick} className="items orange">
+        <button
+          type="button"
+          onClick={buttonClick}
+          className="items orangeColor"
+        >
           =
         </button>
       </div>
